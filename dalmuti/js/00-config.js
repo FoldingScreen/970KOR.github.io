f17dd61b844e5b57600e4fd835158dd5e587f944
leechanyoung <leechanyoung90@gmail.com>
2026-05-21 10:41:14 +0900

    Update stylesheet link in 00-config.js

--- dalmuti/js/00-config.js
+++ dalmuti/js/00-config.js
@@ -272,7 +272,7 @@ function basePlayer(uid, nickname, seatOrder, isAI) {
     const link = document.createElement("link");
     link.id = "dalmutiSingleCss";
     link.rel = "stylesheet";
-    link.href = "./js/00-style.css?v=20260520-tribute-panel1";
+    link.href = "./js/00-style.css?v=20260520-headerline1";
 
     document.head.appendChild(link);
   }
